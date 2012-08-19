795061ac6e1fd43bd133606117b6811e8e08dfc7
cyrus-krc <koorosh.krc@gmail.com>
2012-08-19 16:02:03 +0430

    fix image

--- [zp] Gallery/01 basic animations/gallery.js
+++ [zp] Gallery/01 basic animations/gallery.js
@@ -4,37 +4,5 @@ Math.random = function (a,b){
 }
 
 $(function(){
-	var pics = $('div.gallery a'),
-		maxWidth = window.innerWidth,
-		maxHeight = window.innerHeight,
-		aniDue = 1200,
-		aniStep = 300,
-		aniDelay = 100;
-
-	pics.each(function(){
-		var that = this;
-		setTimeout(function (){
-			$(that).animate({
-				left : Math.random(0,maxWidth - 200 ),
-				top : Math.random(0,maxHeight -200 )
-			},aniDue,'cubic-bezier(.05,.7,.3,.95)');
-
-		},aniDelay);
-		aniDelay += aniStep;
-	});
-
-	pics.click(function(){
-		$(this).animate({
-			left:100,
-			top:100
-		},1000);
-
-		$('img',this).animate({
-			width:500
-		},1000);
-
-		return false;
-	});
-});
-
-
+	// kh kh kh 
+})
